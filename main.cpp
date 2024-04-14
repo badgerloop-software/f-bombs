db4b0dcbfc6284ab3a7c3617e7b7fe1020d6fa06
John Choi <breinv1@gmail.com>
2024-04-14 00:10:23 -0500

    improved format, currently setting up code for testing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include "mbed.h"
+#include "bombs.h"
+#include "candecoder.h"
 
 //testing code
 
 int main()
 {
     
+    BufferedSerial serial(USBTX, USBRX);
+    serial.set_baud(115200);
+    serial.set_blocking(false);
+
+    //test delta_time
+    printf("im alive\n");
+    Bombs bombs = Bombs();
+    bombs.set_net_current(1);
+    while (1) {
+
+        printf("%f\n", bombs.pack_soc());
+        bombs.update_time();
+    }
     return 0;
 }
